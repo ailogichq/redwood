f670ddab5ddb245c93c9c306a489ca36b0c86fe2
James Cantrell <james@seoheap.com>
2023-05-23 16:31:42 -0700

    Cli tools

---
+++ cli/cli.py
@@ -0,0 +1,24 @@
+import argparse
+import sys
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from redwood.redwood import Redwood
+
+# Create an instance of the Redwood class and load data, set a model, and set an agent
+redwood = Redwood()
+
+if (len(sys.argv)>2 and sys.argv[2]):
+    redwood.data(sys.argv[2])
+redwood.model("OpenAI", api_key=os.getenv('OPENAI_API_KEY'))
+if (len(sys.argv)>2 and sys.argv[2]):
+    redwood.agent('CSV')
+else:
+    redwood.agent("Python")
+
+output=redwood.ask(sys.argv[1])
+
+print("Response: ", output)
+
